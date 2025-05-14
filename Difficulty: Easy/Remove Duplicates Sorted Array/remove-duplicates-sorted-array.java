531a05d806c21876cba76aab9207f08f09519fbf
marindave <marindave856@gmail.com>
2025-05-14 08:39:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates Sorted Array/remove-duplicates-sorted-array.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            String str[] = br.readLine().split(" ");
+            int arr[] = new int[str.length];
+            for (int i = 0; i < str.length; i++) arr[i] = Integer.parseInt(str[i]);
+            Solution obj = new Solution();
+            int len = obj.removeDuplicates(arr);
+            for (int i = 0; i < len; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to remove duplicates from the given array
+    public int removeDuplicates(int[] arr) {
+        int n=arr.length;
+       int i=0;
+       int j=1;
+       while(j<n){
+           if(arr[i]!=arr[j]) {
+               i++;
+               arr[i]=arr[j];
+           }
+           arr[i]=arr[j];
+           j++;
+       }
+       return i+1;
+    }
+}
